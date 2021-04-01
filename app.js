b9399408bd0659d0c539d0c0dc6ead18e9aec01f
amulyamallepalli <77821601+amulyamallepalli@users.noreply.github.com>
2021-03-31 21:36:32 -0500

    BONUS

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var TeaRouter = require('./routes/Tea');
-
+var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
 
 var app = express();
 
@@ -24,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Tea', TeaRouter);
+app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
